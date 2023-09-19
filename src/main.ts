ca01bbb824ca7f5e78d5010dc643bcee81478649
zickycoodes <ezekieluduak2@gmail.com>
2023-09-19 12:48:02 +0100

    Cors Issue supposedly fixed

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Sequelize } from 'sequelize-typescript';
+// import { cors } from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const sequelize = app.get<Sequelize>(Sequelize);
+  // const sequelize = app.get<Sequelize>(Sequelize);
   try {
-    await sequelize.authenticate();
+    // await sequelize.authenticate();
     console.log('Connected to the database');
     await app.listen(process.env.PORT || 3000);
     // app.enableCors({
@@ -37,9 +38,9 @@ async function bootstrap() {
     //   preflightContinue: false,
     //   optionsSuccessStatus: 200,
     // });
-
+    // app.use(cors());
     console.log('Application started');
-    await sequelize.sync({});
+    // await sequelize.sync({});
   } catch (err) {
     console.log('Failed to connect to the database:', err);
     return;
